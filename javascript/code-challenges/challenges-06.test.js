43dcd305e3c779a2bdf0e5ff774c155b3e386f54
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-10-25 08:47:43 -0700

    challenge day 6 stretch number 5

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -96,6 +96,11 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
+  people.forEach(obj => {
+    obj.isAuthor = true;
+  });
+
+  people.isAuthor = true;
   // Solution code here...
 };
 
@@ -165,7 +170,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
