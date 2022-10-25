10d96342a8414935e2269028c9bbfdc9a1e24120
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-10-25 08:52:04 -0700

    stretch number 6 done

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -120,6 +120,7 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
+  arr1.push(...arr2);
   // Solution code here...
 
 };
@@ -181,7 +182,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
