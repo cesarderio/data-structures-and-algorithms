781b313c8718a9332862296655d0644b1155ab2b
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-10-28 22:50:57 -0700

    console.log() gets right answer but doesn't pass the test?

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -117,9 +117,13 @@ stores.reduce((acc, arr) => acc.map((sum, i) => sum + arr[i]));
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
-  Object.values(data, hours).forEach((data, hours) => {
-    console.log(`sales: ${data} cookies,time: ${hours}`);
+  let newArray = [];
+  Object.values(data, hours).forEach((data, hours) => { newArray.push(`sales: '${data} cookies', time: '${hours} a.m.'`);
+    return newArray;
+    // newArray.push(`sales: '${data} cookies', time: '${hours} a.m.'`);
   });
+  console.log(newArray);
+  // console.log(`sales: '${data} cookies', time: '${hours} a.m.'`);
 
   // Solution code here...
 };
