bb88381876ed1cfcb4ae41f27ef0197fb50d159d
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-10-29 19:27:10 -0700

    test

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -309,7 +309,7 @@ describe('Testing challenge 8', () => {
   });
 });
 
-describe('Testing challenge 9', () => {
+xdescribe('Testing challenge 9', () => {
   test('It should remove non-integers and return "even" or "odd', () => {
     expect(evenOddNumericValues(['Gregor', 2, 4, 1])).toStrictEqual(['even', 'even', 'odd']);
     expect(evenOddNumericValues(['Gregor', 2, 4, 1]).length).toStrictEqual(3);
