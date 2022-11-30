ee66823c5d80d9c44b94499e2434a9e13fa3cc88
Raphael Chookagian <raphaelchookagian@gmail.com>
2022-11-29 16:27:25 -0800

    taking out map.test class demo file

--- javascript/401/class2/map.js
+++ javascript/401/class2/map.js
@@ -1,13 +1,39 @@
 'use strict';
 
-const map = (arr, callback) => {
-  let newArr = [];
-  // iterate thru each index in the array
-  for(let i = 0; i < arr.length, i++){
-    newArr[i] = callback(arr, i)
-  }
-  return newArr;
-};
+// const map = (arr, callback) => {
+//   let newArr = [];
+//   // iterate thru each index in the array
+//   for(let i = 0; i < arr.length, i++){
+//     newArr[i] = callback(arr, i)
+//   }
+//   return newArr;
+// };
 
 
-module.exports = map;
+// module.exports = map;
+
+
+//TEST ----------------------- Put content below in it's own map.test.js file
+
+// 'use strict';
+
+// const map = require('./map');
+
+// describe('Map function', () => {
+//   test('works as expected', () => {
+//     let arr = [1,2,3,4];
+//     let callback = (val, idx) => {
+//       return val * val;
+//     };
+//     let result = map(arr, callback);
+//     expect(result).toEqual([1,4,9,16]);
+//   });
+//   test('returns an array of same length as input', () => {
+//     let arr = [1,2,3,4];
+//     let callback = (val, idx) => {
+//       return val * val;
+//     };
+//     let result = map(arr, callback);
+//     expect(arr.length).toEqual(result.length);
+//   });
+// });
