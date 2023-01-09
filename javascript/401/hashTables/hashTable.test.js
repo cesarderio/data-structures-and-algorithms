50bd6cb31e2cae25a0c57c9eebb6ed9146c59cc2
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-01-08 17:43:38 -0800

    working on code challenge 30 readme file

---
+++ javascript/401/hashTables/hashTable.test.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+// Setting a key/value to your hashtable results in the value being in the data structure
+// Retrieving based on a key returns the value stored
+// Successfully returns null for a key that does not exist in the hashtable
+// Successfully returns a list of all unique keys that exist in the hashtable
+// Successfully handle a collision within the hashtable
+// Successfully retrieve a value from a bucket within the hashtable that has a collision
+// Successfully hash a key to an in-range value
